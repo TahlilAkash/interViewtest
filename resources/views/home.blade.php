1b4ef37aa68d43d504317d5a9b8ec5b421053cf2
Tahlil Akash <tahliakash@.com>
2025-05-23 23:20:09 +0600

    nav bar design done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ecom Cloth Store</title>
     <link rel="stylesheet" href="{{ asset('/frontend.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
-    <div class="navbar">
-        <div class="logo">
-            <img src="images/logo.png" width="125px" alt="Logo">
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="images/logo.png" width="125px" alt="Logo">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Account</a></li>
+                    </ul>
+                </nav>
+                <img src="images/cart.png" width="25px" height="25px" alt="Cart">
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <h1>Give Your Workout <br> A New Style!</h1>
+                    <p>Success isn't always about greatness. It's about consistency. Consistent
+                        <br> hard work gains success. Greatness will come.
+                    </p>
+                    <a href="#" class="btn">Explore Now &#8594;</a>
+                </div>
+                <div class="col-2">
+                    <img src="images/image1.png" alt="">
+                </div>
+            </div>
         </div>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Account</a></li>
-            </ul>
-        </nav>
     </div>
 </body>
 
